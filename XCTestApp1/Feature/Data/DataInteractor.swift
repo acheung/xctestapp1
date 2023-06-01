03ddf8e9ff31a86a16bd9313c8c0f4cb19651043
Anthony Cheung <acheung@pelmorex.com>
2023-06-01 15:57:05 -0400

    IOS-1: Add pod dependency to source code and post clone script to install pod executable and dependencies

---
+++ XCTestApp1/Feature/Data/DataInteractor.swift
@@ -0,0 +1,21 @@
+//
+//  DataInteractor.swift
+//  XCTestApp1
+//
+//  Created by Anthony Cheung on 2023-06-01.
+//
+
+import Foundation
+import Alamofire
+
+protocol DataInteractorProtocol {
+    func fetch()
+}
+
+class DataInteractor: DataInteractorProtocol {
+    
+    func fetch() {
+        let _ = AF.defaultEventMonitors
+    }
+    
+}
---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Install CocoaPods using Homebrew.
+# brew install cocoapods
+gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
+bundle install
+
+# Install dependencies you manage with CocoaPods.
+bundle exec pod install
