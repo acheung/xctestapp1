9eeddc422998f44972843b60a18ef79b0557b1a3
Anthony Cheung <acheung@pelmorex.com>
2023-06-08 17:22:53 -0400

    IOS-1: Script clean up

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+# tools
 brew install ruby
 export GEM_HOME=$HOME/.gem
 export PATH=$GEM_HOME/bin:$PATH
 gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
-# brew install cocoapods
-# pod install
+
+# dependencies
 bundle install
 bundle exec pod install
