2767769b7c47a579a24fe0b391eac1e0b1c58e5a
Anthony Cheung <acheung@pelmorex.com>
2023-06-08 14:43:39 -0400

    IOS-1: update bundle ID and try different script for pod install

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,8 +2,11 @@
 
 # Install CocoaPods using Homebrew.
 # brew install cocoapods
-gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
-bundle install
+# gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
+# bundle install
 
-# Install dependencies you manage with CocoaPods.
-bundle exec pod install
+# # Install dependencies you manage with CocoaPods.
+# bundle exec pod install
+
+brew install cocoapods
+pod install
