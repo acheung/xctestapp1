3511bba0f61c727cbe771b7fcf822182827df583
Anthony Cheung <acheung@pelmorex.com>
2023-06-08 15:56:30 -0400

    IOS-1: try ruby

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Install CocoaPods using Homebrew.
-# brew install cocoapods
-# gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
-# bundle install
-
-# # Install dependencies you manage with CocoaPods.
-# bundle exec pod install
-
 brew install ruby
-gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
-bundle install
-bundle exec pod install
+brew install cocoapods
+pod install
