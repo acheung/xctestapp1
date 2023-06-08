89291b5eb9f3c44b4317e4bb9216dd50e6c3daeb
Anthony Cheung <acheung@pelmorex.com>
2023-06-08 17:12:08 -0400

    IOS-1: try bundle install and pod install

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,5 +4,7 @@ brew install ruby
 export GEM_HOME=$HOME/.gem
 export PATH=$GEM_HOME/bin:$PATH
 gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
-brew install cocoapods
-pod install
+# brew install cocoapods
+# pod install
+bundle install
+bundle exec pod install
