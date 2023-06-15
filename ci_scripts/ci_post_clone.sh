28dbb3ccd0b79b72a5037006cbd8d020c50e9f91
Anthony Cheung <acheung@pelmorex.com>
2023-06-15 16:38:17 -0400

    IOS-1: Test other ci scripts and add symlink to gemfile.lock

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,8 +2,8 @@
 
 # tools
 brew install ruby
-export GEM_HOME=$HOME/.gem
-export PATH=$GEM_HOME/bin:$PATH
+# export GEM_HOME=$HOME/.gem
+# export PATH=$GEM_HOME/bin:$PATH
 # gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
 
 # bundle install
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# tools
+export GEM_HOME=$HOME/.gem
+export PATH=$GEM_HOME/bin:$PATH
 gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)" || :
-
 bundle install
 
 # dependencies
